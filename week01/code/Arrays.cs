1c31febe8b1fbd5ac6aa2c7087075a451d6e4732
moroninavarro <moronialanis23@gmail.com>
2026-01-07 23:54:05 -0800

    change

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +14,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
 
-        return []; // replace this return statement with your own
+
+        //In this function i need to create the code to return an array starting with 'number' followed by multiples of 'number'.
+        //For example MultiplesOf(3,5), where the 3 is the starting number and 5 is the number of multiples, would result in <double>{3,6,9,12,15}.
+        //step 1 Create name of the array using the 'double' as is required, then we use 'new double[length]' to create the doubles with length spaces in the array.
+        //step 2 Create a loop 'for'with specification, a count variable called 'i' which starts at 0 which is the position of the array
+        //the cicle will repeat it while length is minor then 'i'. Then increase the value of 'i' in 1 after to change the position of the array.
+        //step 3 Create another variable called result[i], it multiply the number by the [i] plus 1.
+            double[] result = new double[length];
+            for(int i = 0; i < length; i++)
+            {
+                result[i] = number * (i + 1);
+            }
+        
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
